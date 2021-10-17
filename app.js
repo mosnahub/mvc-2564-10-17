7f6f8238e66b48191b582cd583d2d871ad26e0fd
Punnarat <punnarat_mos@hotmail.com>
2021-10-17 16:57:45 +0700

    :heavy_plus_sign: Add dotenv and move dbURI to .env

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-const dbURI =
-	'mongodb+srv://mzwallow:1234@cluster0.xwcz4.mongodb.net/mvc?retryWrites=true&w=majority';
 mongoose
-	.connect(dbURI)
+	.connect(process.env.DB_URI)
 	.then((res) =>
 		app.listen(3000, () => {
 			console.log('Running on port 3000');
